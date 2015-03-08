410672eaaa238cdfa7cda6304865366c40dc02c3
Matt Mongeon <matthewmongeon2015@u.northwestern.edu>
2015-03-08 17:56:00 -0500

    Fixed a bunch of issues with matrix sizes and so forth.  SVD is EXTREMELY slow.  So far it is only being done on a single channel...

--- main.cpp
+++ main.cpp
@@ -4,6 +4,12 @@
 #include <string>
 
 
+void printMatrixSize( const cv::Mat& matrix, const std::string& matName )
+{
+	std::cout << matName << " size:  " << matrix.size().height << " x " << matrix.size().width << std::endl;
+}
+
+
 // Takes in the location of a video and a container for video frames and fills
 // the container with the frames from the video.  Returns true if successulf,
 // false otherwise.
@@ -19,7 +25,7 @@ bool readVideoFrames( const std::string& videoLoc, std::vector<cv::Mat>& frames 
 		return false;
 	}
 
-	while(1)
+	while(frames.size() < 100)
 	{
 		cv::Mat frame;
 		
@@ -28,11 +34,13 @@ bool readVideoFrames( const std::string& videoLoc, std::vector<cv::Mat>& frames 
 			break;
 
 		// TODO:  CONVERT FRAME TO CV_32FC3 FOR PROCESSING BELOW
-
-		frames.push_back(frame);
+		
+		// For now read in only a portion of the data so that we can test faster.
+		frames.push_back(frame.rowRange(0, 50).colRange(0, 50));
 	}
 
 	std::cout << "Num frames:  " << frames.size() << std::endl;
+	printMatrixSize( frames[0], "Frame" );
 	std::cout << "Type:  " << frames[0].type() << std::endl;
 	if( frames[0].type() == CV_8UC3 )
 		std::cout << "Type is CV_8UC3" << std::endl;
@@ -60,14 +68,16 @@ void playVideoSequence( const std::vector<cv::Mat>& frames )
 }
 
 
-void convertFramesToVectors( const std::vector<cv::Mat>& frames, cv::Mat& Y )
+void convertFramesToVectors( const std::vector<cv::Mat>& frames, cv::Mat& Y1, cv::Mat& Y2, cv::Mat& Y3 )
 {
 	int width = frames[0].size().width;
 	int height = frames[0].size().height;
 	int m = width*height;
 	int tau = frames.size();
-	Y = cv::Mat(m, tau, CV_32FC3);
-
+	Y1 = cv::Mat(m, tau, CV_32F);
+	Y2 = cv::Mat(m, tau, CV_32F);
+	Y3 = cv::Mat(m, tau, CV_32F);
+	
 	for( std::size_t i = 0; i < frames.size(); ++i )
 	{
 		const cv::Mat& frame = frames[i];
@@ -78,11 +88,9 @@ void convertFramesToVectors( const std::vector<cv::Mat>& frames, cv::Mat& Y )
 			{
 				const cv::Vec3b& oldPixel = frame.at<cv::Vec3b>(row, col);
 
-				cv::Vec3f& newPixel = Y.at<cv::Vec3f>( row*width+col , i );
-
-				newPixel[0] = static_cast<float>(oldPixel[0]);
-				newPixel[1] = static_cast<float>(oldPixel[1]);
-				newPixel[2] = static_cast<float>(oldPixel[2]);
+				Y1.at<cv::Vec3f>( row*width+col , i ) = static_cast<float>(oldPixel[0]);
+				Y2.at<cv::Vec3f>( row*width+col , i ) = static_cast<float>(oldPixel[1]);
+				Y3.at<cv::Vec3f>( row*width+col , i ) = static_cast<float>(oldPixel[2]);
 			}
 		}
 	}
@@ -118,7 +126,7 @@ void convertVectorsToFrames( const cv::Mat& vectors, std::vector<cv::Mat>& frame
 
 
 void learnDynamicTexture( const std::vector<cv::Mat>& frames, int n, int nv,
-						  cv::Mat& x0, cv::Mat& Ymean,
+						  cv::Mat& x0, cv::Mat& Y1mean, cv::Mat& Y2mean, cv::Mat& Y3mean,
 						  cv::Mat& Ahat, cv::Mat& Bhat, cv::Mat& Chat )
 {
 	std::cout << "In learnDynamicTexture" << std::endl;
@@ -127,91 +135,110 @@ void learnDynamicTexture( const std::vector<cv::Mat>& frames, int n, int nv,
 	int m = width * height;
 	int tau = frames.size();
 
+	std::cout << std::endl;
+	std::cout << "tau:  " << tau << std::endl;
+	std::cout << "m:  " << m << std::endl;
+	std::cout << "n:  " << n << std::endl;
+	std::cout << "nv:  " << nv << std::endl;
+	std::cout << std::endl;
 	
 	// --- Average the pixels of all frames --- //
 
 	std::cout << "Calling convertFramesToVectors" << std::endl;
 	// Ymean = mean(Y,2);
-	cv::Mat Y;
-	convertFramesToVectors( frames, Y );
+	cv::Mat Y1;
+	cv::Mat Y2;
+	cv::Mat Y3;
+	convertFramesToVectors( frames, Y1, Y2, Y3 );
 
-	std::cout << "Calculating the average" << std::endl;
+	printMatrixSize( Y1, "Y1" );
+	printMatrixSize( Y2, "Y2" );
+	printMatrixSize( Y3, "Y3" );
+
+	std::cout << "Calculating the averages" << std::endl;
 	
-	Ymean = cv::Mat(m, 1, CV_32FC3);
+	Y1mean = cv::Mat(m, 1, CV_32F);
+	Y2mean = cv::Mat(m, 1, CV_32F);
+	Y3mean = cv::Mat(m, 1, CV_32F);
 
 	for( int row = 0; row < height; ++row )
 	{
-		cv::Vec3f& meanPixel = Ymean.at<cv::Vec3f>(row, 0);
-		meanPixel[0] = 0;
-		meanPixel[1] = 0;
-		meanPixel[2] = 0;
+		float& f1 = Y1mean.at<float>(row, 0);
+		float& f2 = Y2mean.at<float>(row, 0);
+		float& f3 = Y3mean.at<float>(row, 0);
+		f1 = 0;
+		f2 = 0;
+		f3 = 0;
 
 		for( int col = 0; col < width; ++col )
 		{
-			const cv::Vec3f& p = Y.at<cv::Vec3f>(row, col);
-
-			meanPixel[0] += p[0];
-			meanPixel[1] += p[1];
-			meanPixel[2] += p[2];
+			f1 += Y1.at<float>(row,col);
+			f2 += Y2.at<float>(row,col);
+			f3 += Y3.at<float>(row,col);
 		}
 
-		meanPixel /= static_cast<float>(width);
+		f1 /= static_cast<float>(width);
+		f2 /= static_cast<float>(width);
+		f3 /= static_cast<float>(width);
 	}
 
 
-	// --- Singular Value Decomposition --- //
-
-	std::cout << "Singular value decomposition" << std::endl;
-
-	if( Y.type() == CV_32FC3 )
-		std::cout << "\tY type is CV_32FC3" << std::endl;
-
-	if( Ymean.type() == CV_32FC3 )
-		std::cout << "\tYmean type is CV_32FC3" << std::endl;
-
+	// [U,S,V] = svd(Y-Ymean*ones(1,tau),0);
 	std::cout << "Creating ones" << std::endl;
-	cv::Mat ones = cv::Mat::ones(1, tau, CV_32FC3);
-	std::cout << "Ymean * ones" << std::endl;
-	cv::Mat temp = Ymean * ones;
-	std::cout << "Y - Ymean" << std::endl;
-	temp = Y - temp;
+	cv::Mat ones = cv::Mat::ones(1, tau, CV_32F);
+	std::cout << "Y1mean * ones" << std::endl;
+	cv::Mat temp = Y1mean * ones;
+	std::cout << "Y1 - Y1mean" << std::endl;
+	temp = Y1 - temp;
 	
+	std::cout << "Calculating first SVD" << std::endl;
+	printMatrixSize( temp, "temp" );
 	
-		//cv::Mat temp = Y - (Ymean * cv::Mat::ones(1,tau, CV_32FC3));
-	std::cout << "First" << std::endl;
 	cv::SVD svd_temp( temp );
-	// SVD HERE:  [U,S,V] = svd(temp, 0);
-	// U should be m x n
-	// S should be n x n diagonal
-	// V should be tau x n
 
-	// DELETE THIS WHEN SVD WORKS, THIS IS JUST TO GET CODE TO COMPILE
-	// cv::Mat U( m, n, CV_32FC3 );
-	// cv::Mat S( n, n, CV_32FC3 );
-	// cv::Mat V( tau, n, CV_32FC3 );
-	// END DELETE
+	printMatrixSize( svd_temp.u, "svd_temp.u" );
+	printMatrixSize( svd_temp.w, "svd_temp.w" );
+	printMatrixSize( svd_temp.vt, "svd_temp.vt" );
 
 
-	// TEST THE SIZES OF THE MATRICES.
+	cv::Mat sigma = cv::Mat::zeros( n, n, CV_32F );
+	for( int i = 0; i < n; ++i )
+	{
+		sigma.at<float>(i, i) = svd_temp.w.at<float>(i, 0);
+	}
+
+	printMatrixSize( sigma, "Sigma" );
+
+	cv::Mat VT = svd_temp.vt.rowRange(0, n);
+
+	printMatrixSize( VT, "VT" );
 	
-	
-	//Chat = U.colRange(0, n-1);
+	cv::Mat U = svd_temp.u.colRange(0, n);
+
+	printMatrixSize( U, "U" );
+
 	std::cout << "Second" << std::endl;
-	Chat = svd_temp.u;
+	Chat = U;
+	printMatrixSize( Chat, "Chat" );
 
 	// V` <- does this equal V.t()?  Matlab code mentions Hermition transpose...
 	//cv::Mat Xhat( S.colRange(0, n-1).rowRange(0, n-1) * (V.colRange(0,n-1)) );
 	std::cout << "Third" << std::endl;
-	cv::Mat Xhat( svd_temp.w * svd_temp.vt );
+	
+	cv::Mat Xhat( sigma * VT );
+	printMatrixSize( Xhat, "Xhat" );
 
 	std::cout << "Fourth" << std::endl;
 	x0 = Xhat.col(0);
+	printMatrixSize( x0, "x0" );
 
 	std::cout << "Fifth" << std::endl;
 	Ahat = Xhat.colRange(1, tau-1) * (Xhat.colRange(0, tau-2).inv(cv::DECOMP_SVD));
+	printMatrixSize( Ahat, "Ahat" );
 
 	std::cout << "Sixth" << std::endl;
 	cv::Mat Vhat = Xhat.colRange(1, tau-1) - (Ahat * (Xhat.colRange(0, tau-2)));
+	printMatrixSize( Vhat, "Vhat" );
 
 	// [Uv,Sv,Vv] = svd(Vhat,0);
 	std::cout << "Seventh" << std::endl;
@@ -219,9 +246,10 @@ void learnDynamicTexture( const std::vector<cv::Mat>& frames, int n, int nv,
 
 	//Bhat = U.colRange(0, nv-1) * Sv.rowRange(0, nv-1).colRange(0, nv-1);
 	std::cout << "Eighth" << std::endl;
-	Bhat = svd_Vhat.u * svd_Vhat.w;
+	Bhat = U * sigma;
 	std::cout << "Ninth" << std::endl;
 	Bhat /= sqrt(tau-1);
+	printMatrixSize( Bhat, "Bhat" );
 	std::cout << "Exit" << std::endl;
 }
 
@@ -248,18 +276,14 @@ int main( int argc, char** argv )
 		return -1;
 	}
 
-	
-	// DUMMY TEST
 	std::cout << "Calling learnDynamicTexture" << std::endl;
-cv::Mat dummy1;
-cv::Mat dummy2;
-cv::Mat dummy3;
-cv::Mat dummy4;
-cv::Mat dummy5;
-learnDynamicTexture( frames, 0, 0,
-					 dummy1, dummy2,
-					 dummy3, dummy4, dummy5 );	
-std::cout << "Exited learnDynamicTexture" << std::endl;
+
+	cv::Mat x0, Y1mean, Y2mean, Y3mean, Ahat, Bhat, Chat;
+	learnDynamicTexture( frames, 25, 20,
+						 x0, Y1mean, Y2mean, Y3mean,
+						 Ahat, Bhat, Chat );
+
+	std::cout << "Exited learnDynamicTexture" << std::endl;
 
 	playVideoSequence( frames );
 	
